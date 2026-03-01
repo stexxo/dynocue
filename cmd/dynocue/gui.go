533493c5f2131099d69e77df60df9547572a6739
Benjamin Dow <benjamindow1@gmail.com>
2026-03-01 08:33:16 -0500

    version

---
+++ cmd/dynocue/gui.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/wailsapp/wails/v3/pkg/application"
+	"gitlab.com/stexxo/dynocue/frontend"
+)
+
+type Gui struct {
+	app *application.App
+}
+
+func NewGui() *Gui {
+	g := &Gui{}
+
+	g.app = application.New(application.Options{
+		Name:     "DynoCue",
+		Services: []application.Service{},
+		Assets: application.AssetOptions{
+			Handler: application.AssetFileServerFS(frontend.Assets),
+		},
+	})
+
+	g.app.Window.New() // Default Window
+
+	return g
+}
+
+func (g *Gui) Run() error {
+	return g.app.Run()
+}
--- cmd/dynocue/main.go
+++ cmd/dynocue/main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"log/slog"
 	"os"
-
-	"gitlab.com/stexxo/dynocue/dynod/internal/gui"
 )
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	slog.SetDefault(logger)
 
-	err := gui.NewGui().Run()
+	err := NewGui().Run()
 	if err != nil {
 		slog.Error("GUI failed to start", "error", err)
 	}
