f9fecc9e88a1e6821b81177cb3071be7dac32e4a
Benjamin Dow <benjamindow1@gmail.com>
2026-03-18 21:31:33 -0400

    license

--- build/linux/nfpm/scripts/postinstall.sh
+++ build/linux/nfpm/scripts/postinstall.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+
 
 # Update desktop database for .desktop file changes
 # This makes the application appear in application menus and registers its capabilities.
--- frontend/src/lib/stores/cuelists.ts
+++ frontend/src/lib/stores/cuelists.ts
@@ -1,3 +1,11 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import { writable } from 'svelte/store';
 import * as Commands from '../../../bindings/gitlab.com/stexxo/dynocue/internal/gui/commands';
 import { Events } from '@wailsio/runtime';
