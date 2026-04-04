10c9f47c2b702c0dfc78193d52dda6c0a4fc687c
Benjamin Dow <benjamindow1@gmail.com>
2026-04-04 16:14:55 -0400

    try

--- cmd/dynocue/main.go
+++ cmd/dynocue/main.go
@@ -9,18 +9,11 @@ package main
 import (
 	"log/slog"
 	"os"
-
-	"github.com/stexxo/dynocue/internal/gui"
 )
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	slog.SetDefault(logger)
-
-	err := gui.NewGui().Run()
-	if err != nil {
-		slog.Error("GUI failed to start", "error", err)
-	}
 
 	slog.Info("DynoCue has shutdown")
 }
