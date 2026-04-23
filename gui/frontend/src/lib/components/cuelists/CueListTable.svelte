6654306bd8771437f4a49c334d2090b8662e96ab
Benjamin Dow <benjamindow1@gmail.com>
2026-04-22 21:55:12 -0400

    delete

--- gui/frontend/src/lib/components/cuelists/CueListTable.svelte
+++ gui/frontend/src/lib/components/cuelists/CueListTable.svelte
@@ -19,28 +19,41 @@
 		<div class="flex-1 overflow-auto">
 			<table class="table table-pin-rows">
 				<thead class="sticky top-0 z-10 bg-base-100">
-					<tr class="bg-base-100">
-						<th class="w-40">#</th>
-						<th class="min-w-50 max-w-200">Label</th>
-						<th class="min-w-50 max-w-100">Type</th>
-						<th class="min-w-50 max-w-100"></th>
-					</tr>
+				<tr class="bg-base-100">
+					<th class="w-40">#</th>
+					<th class="min-w-50 max-w-200">Label</th>
+					<th class="min-w-50 max-w-100">Type</th>
+					<th class="min-w-50 max-w-100"></th>
+				</tr>
 				</thead>
 				<tbody class="">
-					{#each cuelists as list}
-						<tr>
-							<EditableTableData inputType="number" value={list.number} onSaveEdit={(v)=>{cuelistsStore.renumberCuelist(list.number, v)}} tdClass="w-40"/>
-							<EditableTableData inputType="text" value={list.label} onSaveEdit={(v)=>{cuelistsStore.setLabel(list.number, v)}} tdClass="max-w-200"/>
-							<td>{list.cueListType}</td>
-							<td class="flex flex-row justify-center"><button class="btn btn-outline btn-secondary" onclick={()=>{props.OnOpenCueList(list.number)}}>Open</button></td>
-						</tr>
-					{:else}
-						<tr>
-							<td colspan="3" class="text-center italic text-gray-500">
-								No cue lists found.
-							</td>
-						</tr>
-					{/each}
+				{#each cuelists as list}
+					<tr>
+						<EditableTableData inputType="number" value={list.number} onSaveEdit={(v)=>{cuelistsStore.renumberCueList(list.number, v)}} tdClass="w-40"/>
+						<EditableTableData inputType="text" value={list.label} onSaveEdit={(v)=>{cuelistsStore.setLabel(list.number, v)}} tdClass="max-w-200"/>
+						<td>{list.cueListType}</td>
+						<td class="flex flex-row justify-around">
+							<button class="btn btn-outline btn-secondary" onclick={()=>{props.OnOpenCueList(list.number)}}>Open</button>
+
+							<details class="dropdown">
+								<summary class="btn btn-ghost btn-secondary">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+									</svg>
+								</summary>
+								<ul class="menu dropdown-content">
+									<li><button  class="btn btn-outline btn-accent" onclick={()=>{cuelistsStore.deleteCueList(list.number)}}>Delete</button></li>
+								</ul>
+							</details>
+						</td>
+					</tr>
+				{:else}
+					<tr>
+						<td colspan="3" class="text-center italic text-gray-500">
+							No cue lists found.
+						</td>
+					</tr>
+				{/each}
 				</tbody>
 			</table>
 		</div>
