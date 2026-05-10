6c3ed794551ae8f3a0ea0dc49afde73bf461780f
Benjamin Dow <benjamindow1@gmail.com>
2026-05-10 08:58:33 -0400

    action template

--- components/cues/model/actiontemplates.go
+++ components/cues/model/actiontemplates.go
@@ -9,8 +9,9 @@ import (
 )
 
 var ErrActionTemplateExists = errors.New("action template already exists")
+var ErrActionTemplateNotFound = errors.New("action template not found")
 
-func (m *CueingModel) RegisterActionTemplate(template types.ActionTemplate) error {
+func (m *CueingModel) RegisterActionTemplate(template *types.ActionTemplate) error {
 	err := db.WithWrite(m.runtime, func(txn *memdb.Txn) error {
 		temp, err := txn.First(TableActionTemplates, IndexId, template.TemplateId)
 		if err != nil {
@@ -25,4 +26,15 @@ func (m *CueingModel) RegisterActionTemplate(template types.ActionTemplate) erro
 		return err
 	}
 	return nil
+}
+
+func (m *CueingModel) GetActionTemplateById(templateId string) (*types.ActionTemplate, error) {
+	item, err := db.GetFirstDb[types.ActionTemplate](m.runtime, TableActionTemplates, IndexId, templateId)
+	if err != nil {
+		return nil, err
+	}
+	if item == nil {
+		return nil, ErrActionTemplateNotFound
+	}
+	return item, nil
 }
--- components/cues/model/actiontemplates_test.go
+++ components/cues/model/actiontemplates_test.go
@@ -1,1 +1,36 @@
 package model
+
+import (
+	"testing"
+
+	"github.com/stexxo/dynocue/components/cues/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegisterActionTemplate(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		m, _ := NewCueingModel()
+		err := m.RegisterActionTemplate(&types.ActionTemplate{
+			TemplateId:   "test-template",
+			TemplateName: "test-template",
+			Subject:      "test-subject",
+		})
+		assert.NoError(t, err)
+	})
+
+	t.Run("template id already exists", func(t *testing.T) {
+		m, _ := NewCueingModel()
+		err := m.RegisterActionTemplate(&types.ActionTemplate{
+			TemplateId:   "test-template",
+			TemplateName: "test-template",
+			Subject:      "test-subject",
+		})
+		assert.NoError(t, err)
+		err = m.RegisterActionTemplate(&types.ActionTemplate{
+			TemplateId:   "test-template",
+			TemplateName: "test-template",
+			Subject:      "test-subject",
+		})
+		assert.ErrorIs(t, err, ErrActionTemplateExists)
+	})
+}
